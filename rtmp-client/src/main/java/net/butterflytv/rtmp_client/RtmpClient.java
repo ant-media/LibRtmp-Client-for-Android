3c092d60496d9281afa11cc5d743dc99f8e41ff7
mekya <ahmetmermerkaya@gmail.com>
2016-02-01 13:55:07 +0200

    It now supports all cpu architectures. It is uploaded to bintray

--- rtmp-client/src/main/java/net/butterflytv/rtmp_client/RtmpClient.java
+++ rtmp-client/src/main/java/net/butterflytv/rtmp_client/RtmpClient.java
@@ -12,7 +12,10 @@ public class RtmpClient {
     /**
      * opens the rtmp url
      * @param url
+     * url of the stream
      * @param isPublishMode
+     * if this is an publication it is true,
+     * if connection is for getting stream it is false
      * @return return a minus value if it fails
      * -1 RTMP_Alloc error
      * -2 RTMP_SetupURL error
@@ -23,6 +26,17 @@ public class RtmpClient {
      */
     public native int open(String url, boolean isPublishMode);
 
+    /**
+     * read data from rtmp connection
+     * @param data
+     * buffer that will be filled
+     * @param offset
+     * offset to read data
+     * @param size
+     * size of the data to be reat
+     * @return
+     * number of bytes to be read
+     */
     public native int read(byte[] data, int offset, int size);
 
     public native int write(byte[] data);
@@ -31,6 +45,10 @@ public class RtmpClient {
 
     public native int pause(int pause);
 
+    /**
+     * closes the connection. Dont forget to call
+     * @return 0
+     */
     public native int close();
 
 }
